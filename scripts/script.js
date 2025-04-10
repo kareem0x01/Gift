d8ae4c6df4c6cc6ee61a26bd862ea5940f66235d
kareem0x01 <karimwalidmusic@gmail.com>
2025-04-10 02:53:33 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ const surpriseButton = document.getElementById('surpriseButton');
 
 surpriseButton.addEventListener('click', function() {
     // alert('Happy Birthday! 🎉 Here’s a special surprise just for you!');
-    window.location.href = './game1/index.php';
+    window.location.href = './game1/do_you_love_me.html';
 });
